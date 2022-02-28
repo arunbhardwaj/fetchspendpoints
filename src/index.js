5e85bb99f84fddee1d63cffde0b06fe1437e3bb2
Arun Bhardwaj <arun.b.95@gmail.com>
2022-02-27 21:33:25 -0500

    removes morgan dependency

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const morgan = require('morgan');
 const { balanceRouter, transactionRouter } = require('./routes');
 
 const app = express();
-app.use(morgan('dev'));
 app.use(express.json());
 app.use('/api/balance', (req, res, next) => {
   if (req.method === 'PUT') {
